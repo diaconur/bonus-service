e76570db48a12a1622bbe74408b7503557e1aff3
Razvan <diaconur@gmail.com>
2022-11-27 18:48:27 +0200

    Added improvement for Dsl response

--- src/test/java/com/bonus/client/test/BonusPactTest.java
+++ src/test/java/com/bonus/client/test/BonusPactTest.java
@@ -8,7 +8,13 @@ import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import com.bonus.client.test.dto.BonusResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 @SpringBootTest
 @ExtendWith(PactConsumerTestExt.class)
@@ -31,14 +38,13 @@ public class BonusPactTest {
 
         //Method1 using DslPart from pact
         DslPart bonusResponse1 = new PactDslJsonBody()
-                .maxArrayLike("bonus", 1)
                 .integerType("id", 1)
                 .stringType("bonusName", "SUPER")
                 .stringType("bonusType", "MEGA_BONUS")
-                .stringType("startTime", "2019-10-10")
-                .stringType("endTime", "2019-10-10").closeObject();
+                .stringMatcher("startTime", "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$","2019-10-22")
+                .stringMatcher("endTime","^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$", "2019-10-30");
 
-        //Method2 Reading from json
+//      Method2 Reading from json
         String bonusResponse2 = readFileAsString(System.getProperty("user.dir") + "/src/test/resources/bonus.json");
         log.info("Bonus is {}", bonusResponse2);
 
@@ -49,19 +55,23 @@ public class BonusPactTest {
                 .method("GET")
                 .willRespondWith()
                 .status(200)
-                .body(bonusResponse2)
+                .body("["+bonusResponse1+"]")
                 .toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "getBonus", port = "8081")
-    public void testGetBonuses(MockServer mockServer) {
+    public void testGetBonuses(MockServer mockServer) throws JSONException {
         RestTemplate restTemplate = new RestTemplateBuilder().rootUri(mockServer.getUrl()).build();
 
         ResponseEntity<String> response = restTemplate.getForEntity("/bonus/getAllBonuses", String.class);
         log.info("Response body contains{}", response.getBody());
-        int statusCode = restTemplate.getForEntity("/bonus/getAllBonuses", String.class).getStatusCode().value();
+        JSONArray array = new JSONArray(response.getBody());
+        JSONObject bonus = new JSONObject(array.get(0).toString());
+
+        int statusCode = response.getStatusCode().value();
         Assertions.assertEquals(statusCode, 200);
+        Assertions.assertEquals(bonus.get("bonusType"), "MEGA_BONUS");
     }
 
     public String readFileAsString(String file) throws Exception {
